77a433f0a2bd2626cc87577c793ce8d41f370b2f
Traun Leyden <tleyden@couchbase.com>
2017-02-25 14:45:01 -0800

    Lambda deployment works

--- main.go
+++ main.go
@@ -9,22 +9,11 @@ import (
 	"github.com/goadesign/goa/middleware"
 	"github.com/couchbaselabs/cbbootstrap/goa/app"
 	"github.com/couchbaselabs/cbbootstrap/controllers"
+	"github.com/eawsy/aws-lambda-go-net/service/lambda/runtime/net"
+	"net/http"
 )
 
-func main() {
-
-	/*log.Printf("hello world")
-
-	dynamoDb := cbcluster.CreateDynamoDbSession()
-
-	// create a new CouchbaseNode
-	cbNode := cbcluster.NewCouchbaseNode("foo3", "127.0.0.1", dynamoDb)
-	err := cbNode.CreateOrJoinCuster()
-	if err != nil {
-		panic(fmt.Sprintf("Error: %v", err))
-	}
-
-	log.Printf("done")*/
+func createGoaService() *goa.Service {
 
 	// Create service
 	service := goa.New("CBBootstrap REST API")
@@ -39,6 +28,13 @@ func main() {
 	c := controllers.NewClusterController(service)
 	app.MountClusterController(service, c)
 
+	return service
+}
+
+func main() {
+
+	service := createGoaService()
+
 	// Start service
 	if err := service.ListenAndServe(":8080"); err != nil {
 		service.LogError("startup", "err", err)
@@ -49,28 +45,18 @@ func main() {
 // Handle is the exported handler called by AWS Lambda.
 var Handle apigatewayproxy.Handler
 
-//func init() {
-//	ln := net.Listen()
-//
-//	// Amazon API Gateway Binary support out of the box.
-//	Handle = apigatewayproxy.New(ln, []string{"image/png"}).Handle
-//
-//	// Any Go framework complying with the Go http.Handler interface can be used.
-//	// This includes, but is not limited to, Vanilla Go, Gin, Echo, Gorrila, etc.
-//	go http.Serve(ln, http.HandlerFunc(handle))
-//}
+func init() {
 
-//func handle(w http.ResponseWriter, r *http.Request) {
-//
-//	dynamoDb := cbcluster.CreateDynamoDbSession()
-//
-//	// create a new CouchbaseNode
-//	cbNode := cbcluster.NewCouchbaseNode("foo4", "127.0.0.1", dynamoDb)
-//	err := cbNode.CreateOrJoinCuster()
-//	if err != nil {
-//		w.Write([]byte(err.Error()))
-//	}
-//
-//	w.Write([]byte(fmt.Sprintf("Got cbNode: %+v", cbNode)))
-//
-//}
+	ln := net.Listen()
+
+	// Amazon API Gateway Binary support out of the box.
+	Handle = apigatewayproxy.New(ln, []string{"image/png"}).Handle
+
+	service := createGoaService()
+
+	// Any Go framework complying with the Go http.Handler interface can be used.
+	// This includes, but is not limited to, Vanilla Go, Gin, Echo, Gorrila, etc.
+	go http.Serve(ln, service.Mux)
+
+
+}
